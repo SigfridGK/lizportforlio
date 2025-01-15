0c58de7aea855374c67c05c084f64194ed2a145b
SigfridGK <sigfridacabal3@gmail.com>
2025-01-15 19:13:16 +0800

    UPDATE navigation bar and profile pic

--- js/js1.js
+++ js/js1.js
@@ -1,17 +1,3 @@
 window.addEventListener("scroll", (event) => {
     let scroll = this.scrollY;
-    if (scroll <= 0){
-        $("#customNav").hide();
-    }else{
-        $("#customNav").show();
-    }
-
-    if (scroll > 55 ){
-        $("#addShad").fadeIn(250);
-    }else{
-        $("#addShad").fadeOut(200);
-    }
-
-    var opacityVal = (window.innerHeight - (scroll + 100)) / 100
-    $("#lizBGImg").css('opacity', opacityVal);
 });
--- js/liz-angular-intro.js
+++ js/liz-angular-intro.js
@@ -3,4 +3,6 @@ app.controller('IntroCtrl', function($scope) {
 		"img/swift.png","img/flutter.jpg","img/github.png","img/restapi.png",
 		"img/angular.png","img/nodejs.png","img/sql.jpg","img/aws.jpg"
 	];
+	$scope.date = new Date().getFullYear();
+	$scope.yearExp = $scope.date - 2020;
 });
